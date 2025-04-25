a09272fabcc0afcf3493e268b8558cbb42624cda
ayushka11 <gargayushka@gmail.com>
2025-04-26 00:08:03 +0530

    adds input parser
    Signed-off-by: ayushka11 <gargayushka@gmail.com>

--- backend/Type.hpp
+++ backend/Type.hpp
@@ -1,11 +1,10 @@
 #ifndef TYPE_HPP
 #define TYPE_HPP
 
-#include <string>
 using namespace std;
 
 struct Process {
-    string p_id;
+    int p_id;
     int arrival_time;
     int burst_time;
     int priority;
