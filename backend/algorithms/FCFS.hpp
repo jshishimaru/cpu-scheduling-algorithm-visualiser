3f5038ad4bf1d4b7c25b27ea47a2942ee690dd50
Anmol_Goklani <anmolgoklani24@gmail.com>
2025-04-26 00:39:23 +0530

    add FCFS algorithm

---
+++ backend/algorithms/FCFS.hpp
@@ -0,0 +1,106 @@
+#ifndef FCFS_HPP
+#define FCFS_HPP
+#pragma once
+#include <algorithm>
+#include <vector>
+#include <string>
+#include "../json.hpp"
+#include "../Type.hpp"
+
+using namespace std;
+
+class FCFS {
+public:
+    nlohmann::json schedule(const vector<Process>& processes) {
+        vector<Process> sorted = processes;
+        sort(sorted.begin(), sorted.end(), [](const auto &a, const auto &b) {
+            return a.arrival_time < b.arrival_time;
+        });
+
+        nlohmann::json result;
+        nlohmann::json gantt_chart = nlohmann::json::array();
+        nlohmann::json process_stats = nlohmann::json::array();
+
+        int current_time = 0;
+        vector<int> completion_time(sorted.size(), 0);
+        
+        for (size_t i = 0; i < sorted.size(); i++) {
+            if (current_time < sorted[i].arrival_time) {
+                current_time = sorted[i].arrival_time;
+            }
+            
+            int process_start_time = current_time;
+            int process_end_time = process_start_time + sorted[i].burst_time;
+            
+            vector<int> arrival_points;
+            vector<vector<int>> ready_queues;
+            
+            vector<int> current_ready_queue;
+            for (size_t j = i + 1; j < sorted.size(); j++) {
+                if (sorted[j].arrival_time <= current_time) {
+                    current_ready_queue.push_back(sorted[j].p_id);
+                }
+            }
+            arrival_points.push_back(process_start_time);
+            ready_queues.push_back(current_ready_queue);
+            
+            for (size_t j = 0; j < sorted.size(); j++) {
+                int arrival = sorted[j].arrival_time;
+                if (arrival > process_start_time && arrival < process_end_time) {
+                    arrival_points.push_back(arrival);
+                }
+            }
+            
+            sort(arrival_points.begin(), arrival_points.end());
+            
+            arrival_points.erase(unique(arrival_points.begin(), arrival_points.end()), 
+                               arrival_points.end());
+            
+            for (size_t j = 1; j < arrival_points.size(); j++) {
+                vector<int> ready_queue;
+                for (size_t k = i + 1; k < sorted.size(); k++) {
+                    if (sorted[k].arrival_time <= arrival_points[j]) {
+                        ready_queue.push_back(sorted[k].p_id);
+                    }
+                }
+                ready_queues.push_back(ready_queue);
+            }
+            
+            arrival_points.push_back(process_end_time);
+            
+            for (size_t j = 0; j < arrival_points.size() - 1; j++) {
+                nlohmann::json gantt_row;
+                gantt_row["process_id"] = sorted[i].p_id;
+                gantt_row["start_time"] = arrival_points[j];
+                gantt_row["end_time"] = arrival_points[j + 1];
+                gantt_row["ready_queue"] = ready_queues[j];
+                gantt_chart.push_back(gantt_row);
+            }
+            
+            current_time = process_end_time;
+            completion_time[i] = process_end_time;
+        }
+
+        for (size_t i = 0; i < sorted.size(); i++) {
+            int comp_time = completion_time[i];
+            int turn_around = comp_time - sorted[i].arrival_time;
+            int waiting = turn_around - sorted[i].burst_time;
+
+            nlohmann::json stats_row;
+            stats_row["process_id"] = sorted[i].p_id;
+            stats_row["arrival_time"] = sorted[i].arrival_time;
+            stats_row["burst_time"] = sorted[i].burst_time;
+            stats_row["completion_time"] = comp_time;
+            stats_row["turnaround_time"] = turn_around;
+            stats_row["waiting_time"] = waiting;
+
+            process_stats.push_back(stats_row);
+        }
+
+        result["gantt_chart"] = gantt_chart;
+        result["process_stats"] = process_stats;
+        return result;
+    }
+};
+
+#endif
--- backend/main.cpp
+++ backend/main.cpp
@@ -1,5 +1,10 @@
 #include "Parser.hpp"
+#include "algorithms/FCFS.hpp"
+#include "json.hpp"
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
 int main() {
@@ -7,23 +12,11 @@ int main() {
 
     try {
         vector<Process> processes = Parser::getProcesses(json_file_path);
-        cout << "Processes:\n";
-        for (const auto& process : processes) {
-            cout << "Process ID: " << process.p_id
-                      << ", Arrival Time: " << process.arrival_time
-                      << ", Burst Time: " << process.burst_time
-                      << ", Priority: " << process.priority << "\n";
-        }
 
-        int time_slice = Parser::getTimeSlice(json_file_path);
-        cout << "Time Slice: " << time_slice << "\n";
+        FCFS fcfs;
+        nlohmann::json schedule_result = fcfs.schedule(processes);
 
-        int num_of_queues = Parser::getNumOfQueues(json_file_path);
-        cout << "Number of Queues: " << num_of_queues << "\n";
-
-        string scheduling_algorithm = Parser::getSchedulingAlgorithm(json_file_path);
-        cout << "Scheduling Algorithm: " << scheduling_algorithm << "\n";
-
+        cout << schedule_result.dump(4) << endl;
     } catch (const exception& e) {
         cerr << "Error: " << e.what() << "\n";
     }
